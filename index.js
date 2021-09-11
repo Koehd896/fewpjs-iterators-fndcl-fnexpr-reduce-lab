fc94e972402cf809918a8db6df268d3ef131966c
Raven <ravenkoehler@gmail.com>
2021-09-11 11:27:56 -0400

    commit tree 9138aa6329e750e4fa15d9384cfb7f14e1f0ec95

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function incrementor(previous, current) {
+    return previous += current;
+}
+
+const totalBatteries = batteryBatches.reduce(incrementor, 0);
